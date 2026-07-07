d36e07852bd058f15c068e7e1fbdf0db2fadefcb
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-07 18:15:47 -0300

    Update home.js

--- modules/pages/home.js
+++ modules/pages/home.js
@@ -1,16 +1,10 @@
-import { createHeader } from "../core/header.js";
-import { createNavbar } from "../core/navbar.js";
-import { createFooter } from "../core/footer.js";
+import { createLayout } from "../core/layout.js";
 import { createCard } from "../components/card.js";
 import { ROUTES } from "../../config/routes.js";
 import { createHero } from "../components/hero.js";
 
 export function renderHome() {
-    return `
-        ${createHeader()}
-        ${createNavbar()}
-
-<main>
+return createLayout(`
 
     ${createHero()}
 
@@ -20,34 +14,33 @@ export function renderHome() {
 
         <div class="cards-grid">
 
-        ${createCard(
-            "World Cup",
-            "Fixture, grupos, posiciones y estadísticas.",
-            ROUTES.WORLDCUP
-        )}
-        
-        ${createCard(
-            "Formula 1",
-            "Calendario, pilotos y clasificación.",
-            ROUTES.FORMULA1
-        )}
-        
-        ${createCard(
-            "Noticias",
-            "Últimas novedades deportivas.",
-            ROUTES.NEWS
-        )}
-        
-        ${createCard(
-            "Acerca de",
-            "Conocé más sobre el proyecto EasySports.",
-            ROUTES.ABOUT
-        )}
+            ${createCard(
+                "World Cup",
+                "Fixture, grupos, posiciones y estadísticas.",
+                ROUTES.WORLDCUP
+            )}
+
+            ${createCard(
+                "Formula 1",
+                "Calendario, pilotos y clasificación.",
+                ROUTES.FORMULA1
+            )}
+
+            ${createCard(
+                "Noticias",
+                "Últimas novedades deportivas.",
+                ROUTES.NEWS
+            )}
+
+            ${createCard(
+                "Acerca de",
+                "Conocé más sobre el proyecto EasySports.",
+                ROUTES.ABOUT
+            )}
 
         </div>
-    </section>
-</main>
 
-        ${createFooter()}
-    `;
+    </section>
+
+`);
 }
