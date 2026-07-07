3b26a698ffc41a7750cf1e488e19e93d1d49353d
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-07 18:21:54 -0300

    Add API configuration example for multiple sports

---
+++ config/apis.example.js
@@ -0,0 +1,18 @@
+export const API={
+    FOOTBALL:{
+        BASE_URL:"",
+        TOKEN:""
+    },
+    FORMULA1:{
+        BASE_URL:"",
+        TOKEN:""
+    },
+    MOTOGP:{
+        BASE_URL:"",
+        TOKEN:""
+    },
+    NASCAR:{
+        BASE_URL:"",
+        TOKEN:""
+    }
+};
