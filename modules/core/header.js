e316d8165c46f070cba0ec401481b80c44d30b78
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-07 17:21:18 -0300

    Update header.js

--- modules/core/header.js
+++ modules/core/header.js
@@ -1,1 +1,9 @@
-
+export function createHeader() {
+    return `
+        <header class="header">
+            <div class="logo">
+                ⚽ EasySports
+            </div>
+        </header>
+    `;
+}
