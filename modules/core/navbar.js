b25d9c2c0e6df502a7095f60ef6c8eac653a616b
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-07 17:21:36 -0300

    Add createNavbar function to generate navigation bar

--- modules/core/navbar.js
+++ modules/core/navbar.js
@@ -1,1 +1,11 @@
-
+export function createNavbar() {
+    return `
+        <nav class="navbar">
+            <a href="index.html">Inicio</a>
+            <a href="pages/worldcup.html">World Cup</a>
+            <a href="pages/formula1.html">Formula 1</a>
+            <a href="pages/news.html">Noticias</a>
+            <a href="pages/about.html">Acerca</a>
+        </nav>
+    `;
+}
