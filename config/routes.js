4b86f68cd0996ab7114a28b84ad1fe312fd6a37b
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-07 18:02:29 -0300

    Fix formatting and add getCurrentPage function

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,12 @@
-export const ROUTES = {
-    HOME: "index.html",
-    WORLDCUP: "pages/worldcup.html",
-    FORMULA1: "pages/formula1.html",
-    NEWS: "pages/news.html",
-    ABOUT: "pages/about.html"
+export const ROUTES={
+    HOME:"index.html",
+    WORLDCUP:"pages/worldcup.html",
+    FORMULA1:"pages/formula1.html",
+    NEWS:"pages/news.html",
+    ABOUT:"pages/about.html"
 };
+
+export function getCurrentPage(){
+    const path=window.location.pathname;
+    return path.substring(path.lastIndexOf("/")+1)||"index.html";
+}
