e8fed87939dc55aa1dc82061a7782b007dc91cdb
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-07 17:39:04 -0300

    Update and rename .gitkeep to routes.js

---
+++ config/routes.js
@@ -0,0 +1,7 @@
+export const ROUTES = {
+    HOME: "index.html",
+    WORLDCUP: "pages/worldcup.html",
+    FORMULA1: "pages/formula1.html",
+    NEWS: "pages/news.html",
+    ABOUT: "pages/about.html"
+};
