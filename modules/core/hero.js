dc697d13611a46140238be9d396f05cbf801f4f3
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-07 17:54:41 -0300

    Create hero.js

---
+++ modules/core/hero.js
@@ -0,0 +1,14 @@
+export function createHero(){
+    return `
+        <section class="hero">
+            <div class="hero-content">
+                <h1>EasySports</h1>
+                <p>Toda la información deportiva en un solo lugar.</p>
+                <div class="hero-buttons">
+                    <a href="pages/worldcup.html" class="button">🌍 World Cup</a>
+                    <a href="pages/formula1.html" class="button secondary">🏎️ Formula 1</a>
+                </div>
+            </div>
+        </section>
+    `;
+}
