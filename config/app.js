8f9e373b0f31b3c1ca677509f2b7e613a6742424
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-07 18:25:51 -0300

    Add app configuration with features and settings

---
+++ config/app.js
@@ -0,0 +1,16 @@
+export const APP={
+    NAME:"EasySports",
+    VERSION:"0.1.0",
+    AUTHOR:"Richard",
+    THEME:"dark",
+    LANGUAGE:"es-AR"
+};
+
+export const FEATURES={
+    WORLDCUP:true,
+    FORMULA1:true,
+    MOTOGP:false,
+    NASCAR:false,
+    NBA:false,
+    NFL:false
+};
