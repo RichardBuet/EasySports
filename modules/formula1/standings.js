7703cac0120d3bf6b47608567348e55c0fb78497
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-09 02:18:38 -0300

    Add createF1Standings function for standings display

--- modules/formula1/standings.js
+++ modules/formula1/standings.js
@@ -1,1 +1,20 @@
+export function createF1Standings() {
+    return `
+        <section class="f1-standings">
 
+            <div class="section-header">
+                <h2>🏎 Fórmula 1</h2>
+                <span>Clasificación del Campeonato</span>
+            </div>
+
+            <div id="f1StandingsList" class="standings-list">
+
+                <div class="loading">
+                    Cargando clasificación...
+                </div>
+
+            </div>
+
+        </section>
+    `;
+}
