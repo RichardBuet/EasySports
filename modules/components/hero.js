ccc5eca8c6b583149f8ff4555b8dd875f6382b6d
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-09 03:04:41 -0300

    Refactor createHero function to include banner image
    Removed hero content and added a hero banner image.

--- modules/components/hero.js
+++ modules/components/hero.js
@@ -1,14 +1,13 @@
-export function createHero(){
+export function createHero() {
     return `
         <section class="hero">
-            <div class="hero-content">
-                <h1>EasySports</h1>
-                <p>Toda la información deportiva en un solo lugar.</p>
-                <div class="hero-buttons">
-                    <a href="pages/worldcup.html" class="button">🌍 World Cup</a>
-                    <a href="pages/formula1.html" class="button secondary">🏎️ Formula 1</a>
-                </div>
-            </div>
+
+            <img
+                src="assets/images/bannerTop01.jpg"
+                alt="EasySports Banner"
+                class="hero-banner"
+            >
+
         </section>
     `;
 }
