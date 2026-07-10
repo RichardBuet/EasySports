0558097ef9292a38d9d3a8c7320869b1f14367a1
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-10 18:50:11 -0300

    Refactor navbar to include dynamic routes and sports menu
    Updated navbar links to use dynamic routing and added sports menu.

--- modules/core/navbar.js
+++ modules/core/navbar.js
@@ -1,35 +1,37 @@
 import { ROUTES,getCurrentPage } from "../../config/routes.js";
+import { createSportsMenu } from "./sportsMenu.js";
 
 export function createNavbar(){
 
     const current=getCurrentPage();
-
     const active=(page)=>current===page?"active":"";
 
     return `
         <nav class="navbar">
 
-    <a class="active">
-        🏠
-        <span>Inicio</span>
-    </a>
+            <a href="${ROUTES.HOME}" class="${active("home")}">
+                🏠
+                <span>Inicio</span>
+            </a>
 
-    <a id="sportsMenu">
-        🏅
-        <span>Deportes ▼</span>
-    </a>
+            <a id="sportsMenu">
+                🏅
+                <span>Deportes ▼</span>
+            </a>
 
-    <a>
-        📰
-        <span>Noticias</span>
-    </a>
+            <a href="${ROUTES.NEWS}" class="${active("news")}">
+                📰
+                <span>Noticias</span>
+            </a>
 
-    <a>
-        ℹ️
-        <span>Acerca</span>
-    </a>
+            <a href="${ROUTES.ABOUT}" class="${active("about")}">
+                ℹ️
+                <span>Acerca</span>
+            </a>
 
-</nav>
+        </nav>
+
+        ${createSportsMenu()}
     `;
 
 }
