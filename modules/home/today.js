f372393df5eac522ead73c7dfa85ab13a9da41b1
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-09 21:55:59 -0300

    Update today.js

--- modules/home/today.js
+++ modules/home/today.js
@@ -1,37 +1,35 @@
+import { createWidget } from "../components/widget.js";
+
 export function createToday() {
 
-    return `
-    
-        <section class="dashboard-widget">
+    const content = `
 
-            <h2>📅 Hoy</h2>
+        <div class="today-list">
 
-            <div class="today-list">
-
-                <div class="today-row">
-                    <span>09:00</span>
-                    <span>🏎 FP2 • Bélgica</span>
-                </div>
-
-                <div class="today-row">
-                    <span>13:30</span>
-                    <span>🏍 Clasificación • Alemania</span>
-                </div>
-
-                <div class="today-row">
-                    <span>18:00</span>
-                    <span>⚽ Argentina vs Brasil</span>
-                </div>
-
-                <div class="today-row">
-                    <span>21:30</span>
-                    <span>🏀 Lakers vs Celtics</span>
-                </div>
-
+            <div class="today-row">
+                <span>09:00</span>
+                <span>🏎 FP2 • Bélgica</span>
             </div>
 
-        </section>
+            <div class="today-row">
+                <span>13:30</span>
+                <span>🏍 Clasificación • Alemania</span>
+            </div>
+
+            <div class="today-row">
+                <span>18:00</span>
+                <span>⚽ Argentina vs Brasil</span>
+            </div>
+
+            <div class="today-row">
+                <span>21:30</span>
+                <span>🏀 Lakers vs Celtics</span>
+            </div>
+
+        </div>
 
     `;
+
+    return createWidget("📅 Hoy", content);
 
 }
