549af322bb5508e8485ecf450cb0fb5c553cc77e
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-10 18:47:54 -0300

    Create sportsMenu.js

---
+++ modules/core/sportsMenu.js
@@ -0,0 +1,47 @@
+export function createSportsMenu(){
+
+    return `
+
+        <div id="sportsMenuPanel" class="sports-menu hidden">
+
+            <a href="#">
+                🏎 Fórmula 1
+            </a>
+
+            <a href="#">
+                🏍 MotoGP
+            </a>
+
+            <a href="#">
+                ⚽ Copa del Mundo
+            </a>
+
+            <a href="#">
+                🏁 NASCAR
+            </a>
+
+            <a href="#">
+                🚗 IndyCar
+            </a>
+
+            <a href="#">
+                ⚡ Fórmula E
+            </a>
+
+            <a href="#">
+                🏆 WEC
+            </a>
+
+            <a href="#">
+                🏀 NBA
+            </a>
+
+            <a href="#">
+                🎾 Tenis
+            </a>
+
+        </div>
+
+    `;
+
+}
