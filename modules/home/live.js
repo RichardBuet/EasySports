894cb26686d34d8b6ebc78dbbbf7326ee223e985
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-09 23:44:41 -0300

    Update live.js

--- modules/home/live.js
+++ modules/home/live.js
@@ -1,1 +1,35 @@
+import { createWidget } from "../components/widget.js";
 
+export function createLive(){
+
+    const content=`
+
+        <div class="live-list">
+
+            <div class="live-row">
+                <span class="live-status">🔴 EN VIVO</span>
+                <span>🏎 FP2 • Bélgica</span>
+            </div>
+
+            <div class="live-row">
+                <span class="live-status">78'</span>
+                <span>⚽ Argentina 2-1 Brasil</span>
+            </div>
+
+            <div class="live-row">
+                <span class="live-status">Q4</span>
+                <span>🏀 Lakers 89-84 Celtics</span>
+            </div>
+
+            <div class="live-row">
+                <span class="live-status">10:30</span>
+                <span>🏍 Qualifying • Alemania</span>
+            </div>
+
+        </div>
+
+    `;
+
+    return createWidget("🔴 En Vivo",content);
+
+}
