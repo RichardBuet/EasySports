4b5e307f5f5e8302bd068ba5150be6c1b2b3e257
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 01:02:43 -0300

    Refactor fetchJSON to remove options parameter

--- services/shared/fetch.js
+++ services/shared/fetch.js
@@ -1,11 +1,26 @@
-export async function fetchJSON(url,options={}){
+export async function fetchJSON(url){
 
-    const response=await fetch(url,options);
+    const response=await fetch(url);
 
     if(!response.ok){
+
         throw new Error(`HTTP ${response.status}`);
+
     }
 
     return await response.json();
 
 }
+
+
+// export async function fetchJSON(url,options={}){
+
+//     const response=await fetch(url,options);
+
+//     if(!response.ok){
+//         throw new Error(`HTTP ${response.status}`);
+//     }
+
+//     return await response.json();
+
+// }
