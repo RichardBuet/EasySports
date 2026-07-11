ec95d3c03a08b481b79ffe93b7272d8e2e9e0f86
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 12:07:37 -0300

    Rename methods for driver and constructor standings

--- services/espn/formula1.js
+++ services/espn/formula1.js
@@ -2,31 +2,17 @@ import {ESPNCore} from "./core.js";
 import {adaptDriverStandings} from "../../adapters/formula1Adapter.js";
 
 export class ESPNFormula1{
-
-static async getStandings(){
-
+static async getDriverStandings(){
 const data=await ESPNCore.getDriverStandings();
-
 return adaptDriverStandings(data);
-
 }
-
-static async getConstructors(){
-
+static async getConstructorStandings(){
 return await ESPNCore.getConstructorStandings();
-
 }
-
 static async getCalendar(){
-
 return await ESPNCore.getCalendar();
-
 }
-
 static async getEvents(){
-
 return await ESPNCore.getEvents();
-
 }
-
 }
