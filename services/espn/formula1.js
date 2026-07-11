f9a4edb08e61cf3f75d3c6437f040f94f586b759
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 09:57:34 -0300

    Refactor ESPNFormula1 methods for clarity

--- services/espn/formula1.js
+++ services/espn/formula1.js
@@ -1,16 +1,33 @@
-import { ESPNCore } from "./core.js";
+import {ESPNCore} from "./core.js";
 
 export class ESPNFormula1{
-    static async getDriverStandings(){
-        return await ESPNCore.getDriverStandings();
+
+    static async getStandings(){
+
+        const data=await ESPNCore.getDriverStandings();
+
+        return data.standings;
+
     }
-    static async getConstructorStandings(){
-        return await ESPNCore.getConstructorStandings();
+
+    static async getConstructors(){
+
+        const data=await ESPNCore.getConstructorStandings();
+
+        return data.standings;
+
     }
+
     static async getCalendar(){
+
         return await ESPNCore.getCalendar();
+
     }
+
     static async getEvents(){
+
         return await ESPNCore.getEvents();
+
     }
+
 }
