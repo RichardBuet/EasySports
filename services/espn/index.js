7c6e6e63955d1dbea31a0f6694bb1b5ee5285926
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 01:43:01 -0300

    Uncomment ESPNFormula1 import and export

--- services/espn/index.js
+++ services/espn/index.js
@@ -1,9 +1,9 @@
 import { ESPNCore } from "./core.js";
 import { ESPNSite } from "./site.js";
-// import { ESPNFormula1 } from "./formula1.js";
+import { ESPNFormula1 } from "./formula1.js";
 
 export {
     ESPNCore,
     ESPNSite,
-    // ESPNFormula1
+    ESPNFormula1
 };
