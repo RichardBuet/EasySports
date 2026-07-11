9ba81867d60b3e2c69a01050104999b6cd94a62d
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 19:38:03 -0300

    Refactor ESPNFormula1 methods for constructor standings

--- services/espn/formula1.js
+++ services/espn/formula1.js
@@ -1,5 +1,6 @@
 import {ESPNCore} from "./core.js";
 import {adaptDriverStandings} from "../../adapters/formula1Adapter.js";
+import {adaptConstructorStandings} from "../../adapters/formula1ConstructorAdapter.js";
 
 export class ESPNFormula1{
 
@@ -7,20 +8,28 @@ static async getDriverStandings(){
 
 const data=await ESPNCore.getDriverStandings();
 
-return await adaptDriverStandings(data);
+return adaptDriverStandings(data);
 
 }
 
 static async getConstructorStandings(){
-return await ESPNCore.getConstructorStandings();
+
+const data=await ESPNCore.getConstructorStandings();
+
+return adaptConstructorStandings(data);
+
 }
 
 static async getCalendar(){
+
 return await ESPNCore.getCalendar();
+
 }
 
 static async getEvents(){
+
 return await ESPNCore.getEvents();
+
 }
 
 }
