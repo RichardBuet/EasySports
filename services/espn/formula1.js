e067c1b7fee53115ce59e7b49098b00260082846
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 14:37:13 -0300

    Refactor getDriverStandings to use await consistently

--- services/espn/formula1.js
+++ services/espn/formula1.js
@@ -2,17 +2,25 @@ import {ESPNCore} from "./core.js";
 import {adaptDriverStandings} from "../../adapters/formula1Adapter.js";
 
 export class ESPNFormula1{
+
 static async getDriverStandings(){
+
 const data=await ESPNCore.getDriverStandings();
-return adaptDriverStandings(data);
+
+return await adaptDriverStandings(data);
+
 }
+
 static async getConstructorStandings(){
 return await ESPNCore.getConstructorStandings();
 }
+
 static async getCalendar(){
 return await ESPNCore.getCalendar();
 }
+
 static async getEvents(){
 return await ESPNCore.getEvents();
 }
+
 }
