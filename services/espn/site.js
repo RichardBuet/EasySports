7c0ba09bdecda3284a8590dd3e2cdb4e17a9e7db
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 01:21:14 -0300

    Add ESPNSite class with getNews method

--- services/espn/site.js
+++ services/espn/site.js
@@ -1,1 +1,15 @@
+import { fetchJSON } from "../shared/fetch.js";
 
+const BASE_URL="https://site.api.espn.com/apis/site/v2/sports/racing/f1";
+
+export class ESPNSite{
+
+    static async getNews(){
+
+        return await fetchJSON(
+            `${BASE_URL}/news`
+        );
+
+    }
+
+}
