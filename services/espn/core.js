5926e41fa335e3ca025c691bbda0efb962572474
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 01:20:40 -0300

    Add methods to fetch calendar and events from ESPN API

--- services/espn/core.js
+++ services/espn/core.js
@@ -1,9 +1,41 @@
+// import { fetchJSON } from "../shared/fetch.js";
+// const BASE_URL="https://sports.core.api.espn.com/v2/sports/racing/leagues/f1";
+// export class ESPNCore{
+//     static async getDriverStandings(){
+//         return await fetchJSON(
+//             `${BASE_URL}/seasons/2026/types/2/standings/0`
+//         );
+//     }
+// }
+
 import { fetchJSON } from "../shared/fetch.js";
+
 const BASE_URL="https://sports.core.api.espn.com/v2/sports/racing/leagues/f1";
+
 export class ESPNCore{
+
     static async getDriverStandings(){
+
         return await fetchJSON(
             `${BASE_URL}/seasons/2026/types/2/standings/0`
         );
+
     }
+
+    static async getCalendar(){
+
+        return await fetchJSON(
+            `${BASE_URL}/calendar`
+        );
+
+    }
+
+    static async getEvents(){
+
+        return await fetchJSON(
+            `${BASE_URL}/events`
+        );
+
+    }
+
 }
