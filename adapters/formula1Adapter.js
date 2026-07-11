b25300d480a8b52e01318e865ff6032e1b202658
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 18:37:34 -0300

    Refactor adaptDriverStandings function
    Refactor adaptDriverStandings to use map instead of for loop and return standings directly.

--- adapters/formula1Adapter.js
+++ adapters/formula1Adapter.js
@@ -2,28 +2,19 @@ import {ESPNCore} from "../services/espn/core.js";
 
 export async function adaptDriverStandings(data){
 
-const standings=[];
+return data.standings.map(item=>{
 
-for(const item of data.standings){
-
-const athlete=await ESPNCore.getByUrl(item.athlete.$ref);
 const stats=item.records[0].stats;
 
-standings.push({
+return{
 
+id:item.athlete.$ref.split("/").slice(-1)[0].split("?")[0],
 rank:stats.find(s=>s.name==="rank")?.value,
-driver:athlete.displayName,
-shortName:athlete.shortName,
 points:stats.find(s=>s.name==="championshipPts")?.value,
-wins:stats.find(s=>s.name==="wins")?.value,
-country:athlete.country?.name,
-flag:athlete.flag?.href,
-headshot:athlete.headshot?.href
+wins:stats.find(s=>s.name==="wins")?.value
+
+};
 
 });
-
-}
-
-return standings;
 
 }
