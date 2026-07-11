0685ae3cac15df9fedceaeb47e2a76bb3158c14a
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 19:31:28 -0300

    Add constructor standings adapter function

---
+++ adapters/formula1ConstructorAdapter.js
@@ -0,0 +1,18 @@
+export function adaptConstructorStandings(data){
+
+return data.standings.map(item=>{
+
+const stats=item.records[0].stats;
+
+return{
+
+rank:stats.find(s=>s.name==="rank")?.value,
+points:stats.find(s=>s.name==="points")?.value,
+wins:stats.find(s=>s.name==="wins")?.value,
+teamId:item.team.$ref.split("/").slice(-1)[0].split("?")[0]
+
+};
+
+});
+
+}
