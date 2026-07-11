780fe205511ffd50ea48d1af2a1ea11ef7d69e57
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 02:03:14 -0300

    Update core.js

--- services/espn/core.js
+++ services/espn/core.js
@@ -1,41 +1,10 @@
-// import { fetchJSON } from "../shared/fetch.js";
-// const BASE_URL="https://sports.core.api.espn.com/v2/sports/racing/leagues/f1";
-// export class ESPNCore{
-//     static async getDriverStandings(){
-//         return await fetchJSON(
-//             `${BASE_URL}/seasons/2026/types/2/standings/0`
-//         );
-//     }
-// }
-
 import { fetchJSON } from "../shared/fetch.js";
-
 const BASE_URL="https://sports.core.api.espn.com/v2/sports/racing/leagues/f1";
 
 export class ESPNCore{
-
-    static async getDriverStandings(){
-
-        return await fetchJSON(
-            `${BASE_URL}/seasons/2026/types/2/standings/0`
-        );
-
-    }
-
-    static async getCalendar(){
-
-        return await fetchJSON(
-            `${BASE_URL}/calendar`
-        );
-
-    }
-
-    static async getEvents(){
-
-        return await fetchJSON(
-            `${BASE_URL}/events`
-        );
-
-    }
-
+    static async get(path){return await fetchJSON(`${BASE_URL}${path}`);}
+    static async getDriverStandings(){return await this.get("/seasons/2026/types/2/standings/0");}
+    static async getConstructorStandings(){return await this.get("/seasons/2026/types/2/standings/1");}
+    static async getCalendar(){return await this.get("/calendar");}
+    static async getEvents(){ return await this.get("/events");}
 }
