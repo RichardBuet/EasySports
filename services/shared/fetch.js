c229278d4c31ec2596269fdfec4954ae0dbd07df
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 01:07:15 -0300

    Update fetch.js

--- services/shared/fetch.js
+++ services/shared/fetch.js
@@ -1,9 +1,7 @@
 export async function fetchJSON(url){
-
-    const response=await fetch(url);
+  const response=await fetch(url);
 
     if(!response.ok){
-
         throw new Error(`HTTP ${response.status}`);
 
     }
