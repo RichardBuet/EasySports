6b588595689d7ec51e6e0926585403d4fb2af462
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 01:19:42 -0300

    Enhance test.js with more ESPN data logging
    Added additional logging for calendar, events, and news.

--- services/test.js
+++ services/test.js
@@ -2,8 +2,26 @@
 // console.log(await ESPNCore.getDriverStandings());
 
 import { ESPNCore } from "./espn/index.js";
+import { ESPNSite } from "./espn/index.js";
 
 (async()=>{
-    const data=await ESPNCore.getDriverStandings();
-    console.log(data);
+
+    console.clear();
+
+    console.log("================================");
+    console.log("      EasySports Lab");
+    console.log("================================");
+
+    console.log("🏆 Driver Standings");
+    console.log(await ESPNCore.getDriverStandings());
+
+    console.log("📅 Calendar");
+    console.log(await ESPNCore.getCalendar());
+
+    console.log("🏁 Events");
+    console.log(await ESPNCore.getEvents());
+
+    console.log("📰 News");
+    console.log(await ESPNSite.getNews());
+
 })();
