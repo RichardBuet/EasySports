957057d4c37ac85bd1bdeccb2de2178148db8092
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 02:52:45 -0300

    Add explorer.js with button click functionality

---
+++ dev/explorer.js
@@ -0,0 +1,14 @@
+import {ESPNCore,ESPNSite} from "../services/espn/index.js";
+
+const output=document.getElementById("output");
+const button=document.getElementById("load");
+
+console.log("Explorer cargado");
+
+button.onclick=async()=>{
+
+console.log("Botón presionado");
+
+alert("Botón OK");
+
+};
