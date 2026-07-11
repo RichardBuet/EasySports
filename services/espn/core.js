298fc0affbd84a3bec4d189bced3cf697ecfa431
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 01:03:56 -0300

    Add ESPNCore class to fetch F1 driver standings

--- services/espn/core.js
+++ services/espn/core.js
@@ -1,1 +1,9 @@
-
+import { fetchJSON } from "../shared/fetch.js";
+const BASE_URL="https://sports.core.api.espn.com/v2/sports/racing/leagues/f1";
+export class ESPNCore{
+    static async getDriverStandings(){
+        return await fetchJSON(
+            `${BASE_URL}/seasons/2026/types/2/standings/0`
+        );
+    }
+}
