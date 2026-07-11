6f6f12626a6f1bff4cd3b1241fe1397e6d08932a
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-11 14:36:18 -0300

    Update formula1Adapter.js

--- adapters/formula1Adapter.js
+++ adapters/formula1Adapter.js
@@ -1,18 +1,29 @@
-export function adaptDriverStandings(data){
+import {ESPNCore} from "../services/espn/core.js";
 
-return data.standings.map(item=>{
+export async function adaptDriverStandings(data){
 
+const standings=[];
+
+for(const item of data.standings){
+
+const athlete=await ESPNCore.getByUrl(item.athlete.$ref);
 const stats=item.records[0].stats;
 
-return{
+standings.push({
 
 rank:stats.find(s=>s.name==="rank")?.value,
+driver:athlete.displayName,
+shortName:athlete.shortName,
 points:stats.find(s=>s.name==="championshipPts")?.value,
 wins:stats.find(s=>s.name==="wins")?.value,
-athlete:item.athlete.$ref
-
-};
+country:athlete.country?.name,
+flag:athlete.flag?.href,
+headshot:athlete.headshot?.href
 
 });
+
+}
+
+return standings;
 
 }
