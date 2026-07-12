2dd54759fca1937870032f2dc97f997a117a9299
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-12 02:30:32 -0300

    Add debug logs in adaptConstructorStandings function
    Added console logs for debugging constructor standings adaptation.

--- adapters/formula1ConstructorAdapter.js
+++ adapters/formula1ConstructorAdapter.js
@@ -1,27 +1,29 @@
 import {ESPNCore} from "../services/espn/core.js";
 
 export async function adaptConstructorStandings(data){
+console.log("DATA",data);
+console.log("PRIMER ITEM",data.standings[0]);
+  
+// const standings=[];
 
-const standings=[];
+// for(const item of data.standings){
 
-for(const item of data.standings){
+// console.log(item);
 
-console.log(item);
+// const manufacturer=await ESPNCore.getByUrl(item.manufacturer.$ref);
 
-const manufacturer=await ESPNCore.getByUrl(item.manufacturer.$ref);
+// console.log(manufacturer);
 
-console.log(manufacturer);
+// const stats=item.records[0].stats;
 
-const stats=item.records[0].stats;
+// standings.push({
 
-standings.push({
+// rank:stats.find(s=>s.name==="rank")?.value,
+// manufacturer
 
-rank:stats.find(s=>s.name==="rank")?.value,
-manufacturer
+// });
 
-});
-
-}
+// }
 
 return standings;
 
