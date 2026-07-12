a028525cfbbc206e75c8c7fc0035fdb0c511eca4
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-12 02:19:02 -0300

    Update formula1ConstructorAdapter.js

--- adapters/formula1ConstructorAdapter.js
+++ adapters/formula1ConstructorAdapter.js
@@ -8,15 +8,14 @@ for(const item of data.standings){
 
 const manufacturer=await ESPNCore.getByUrl(item.manufacturer.$ref);
 
+console.log(manufacturer);
+
 const stats=item.records[0].stats;
 
 standings.push({
 
 rank:stats.find(s=>s.name==="rank")?.value,
-constructor:manufacturer.displayName,
-points:stats.find(s=>s.name==="points")?.value,
-wins:stats.find(s=>s.name==="wins")?.value,
-logo:manufacturer.logo?.href
+manufacturer
 
 });
 
