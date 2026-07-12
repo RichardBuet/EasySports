f29380c537f75095f173401b1c41756e242f34f2
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-12 02:34:02 -0300

    Introduce new error throw in formula1ConstructorAdapter
    Added an error throw statement for the new adapter.

--- adapters/formula1ConstructorAdapter.js
+++ adapters/formula1ConstructorAdapter.js
@@ -1,31 +1,6 @@
 import {ESPNCore} from "../services/espn/core.js";
-
+throw new Error("ESTE ES EL ADAPTER NUEVO");
 export async function adaptConstructorStandings(data){
 console.log("DATA",data);
-console.log("PRIMER ITEM",data.standings[0]);
 return data;
 }
-// const standings=[];
-
-// for(const item of data.standings){
-
-// console.log(item);
-
-// const manufacturer=await ESPNCore.getByUrl(item.manufacturer.$ref);
-
-// console.log(manufacturer);
-
-// const stats=item.records[0].stats;
-
-// standings.push({
-
-// rank:stats.find(s=>s.name==="rank")?.value,
-// manufacturer
-
-// });
-
-// }
-
-// return standings;
-
-// }
