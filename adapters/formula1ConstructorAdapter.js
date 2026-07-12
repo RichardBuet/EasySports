bacd3e26808a6886a05d1e29736698b2989d89f0
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-12 02:15:43 -0300

    Refactor adaptConstructorStandings to use async/await

--- adapters/formula1ConstructorAdapter.js
+++ adapters/formula1ConstructorAdapter.js
@@ -1,7 +1,27 @@
-export function adaptConstructorStandings(data){
+import {ESPNCore} from "../services/espn/core.js";
 
-console.log(data);
+export async function adaptConstructorStandings(data){
 
-return data;
+const standings=[];
+
+for(const item of data.standings){
+
+const manufacturer=await ESPNCore.getByUrl(item.manufacturer.$ref);
+
+const stats=item.records[0].stats;
+
+standings.push({
+
+rank:stats.find(s=>s.name==="rank")?.value,
+constructor:manufacturer.displayName,
+points:stats.find(s=>s.name==="points")?.value,
+wins:stats.find(s=>s.name==="wins")?.value,
+logo:manufacturer.logo?.href
+
+});
+
+}
+
+return standings;
 
 }
