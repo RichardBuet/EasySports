0bc620853dc80f78fd1ac00adea3405d58041489
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-13 12:51:36 -0300

    Refactor NASCARResults class and getResults method
    Refactor NASCARResults class and update getResults method to throw an error for unimplemented functionality.

--- services/nascar/results.js
+++ services/nascar/results.js
@@ -1,13 +1,11 @@
 import { fetchJSON } from "../shared/fetch.js";
 
-const BASE_URL = "https://feed.nascar.com/api";
+export class NASCARResults {
 
-export class NASCARResults{
+    static async getResults() {
 
-    static async getResults(seriesId=1,season=2026){
-
-        return await fetchJSON(
-            `${BASE_URL}/races/results?series_id=${seriesId}&season=${season}`
+        throw new Error(
+            "NASCAR Results aún no implementado. Utilizaremos weekend-feed.json."
         );
 
     }
