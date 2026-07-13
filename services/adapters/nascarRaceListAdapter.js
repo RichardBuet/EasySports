ac70fa34960052418bd320dbdc44c959b7b5f768
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-13 13:01:32 -0300

    Update nascarRaceListAdapter.js

--- services/adapters/nascarRaceListAdapter.js
+++ services/adapters/nascarRaceListAdapter.js
@@ -1,13 +1,7 @@
 export function adaptNascarRaceList(data) {
 
-    return data.map(race => ({
+    console.log(data);
 
-        raceId: race.race_id,
-        name: race.race_name,
-        track: race.track_name,
-        date: race.race_date,
-        completed: race.race_status === "FINAL"
-
-    }));
+    return data;
 
 }
