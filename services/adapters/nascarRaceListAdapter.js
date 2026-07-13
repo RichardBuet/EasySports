94738282313460a4b5bc857530ad876290db2815
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-13 15:32:06 -0300

    Rename function and update race data mapping

--- services/adapters/nascarRaceListAdapter.js
+++ services/adapters/nascarRaceListAdapter.js
@@ -1,41 +1,24 @@
-export function adaptNascarWeekend(data) {
+export function adaptNascarRaceList(data) {
 
-    const race = data.weekend_race[0];
-
-    return {
+    return data.map(race => ({
 
         raceId: race.race_id,
-        race: race.race_name,
+
+        name: race.race_name,
+
         track: race.track_name,
+
         date: race.race_date,
 
-        winner: race.results[0].driver_fullname,
-        car: race.results[0].car_number,
-        manufacturer: race.results[0].car_make,
-        team: race.results[0].team_name,
+        scheduledLaps: race.scheduled_laps,
 
-        margin: race.margin_of_victory,
-        cautions: race.number_of_cautions,
-        cautionLaps: race.number_of_caution_laps,
-        averageSpeed: race.average_speed,
+        completed: race.actual_laps > 0,
 
-        leaderboard: race.results
-            .sort((a, b) => a.finishing_position - b.finishing_position)
-            .map(driver => ({
+        winner:
+            race.actual_laps > 0 && race.race_comments
+                ? race.race_comments.split(" wins")[0]
+                : "-"
 
-                position: driver.finishing_position,
-                number: driver.car_number,
-                driver: driver.driver_fullname,
-                manufacturer: driver.car_make,
-                team: driver.team_name,
-                sponsor: driver.sponsor,
-                started: driver.starting_position,
-                lapsLed: driver.laps_led,
-                points: driver.points_earned,
-                status: driver.finishing_status
-
-            }))
-
-    };
+    }));
 
 }
