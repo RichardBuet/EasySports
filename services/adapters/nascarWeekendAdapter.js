0b1f0f0070f8e0df4b728278292a198e95e09027
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-13 14:27:02 -0300

    Refactor adaptNascarWeekend to return detailed race info

--- services/adapters/nascarWeekendAdapter.js
+++ services/adapters/nascarWeekendAdapter.js
@@ -1,5 +1,38 @@
 export function adaptNascarWeekend(data) {
 
-    return data.weekend_race[0];
+    const race = data.weekend_race[0];
+
+    return {
+
+        raceId: race.race_id,
+        race: race.race_name,
+        track: race.track_name,
+        date: race.race_date,
+
+        winner: race.results[0].driver_fullname,
+        car: race.results[0].car_number,
+        manufacturer: race.results[0].car_make,
+        team: race.results[0].team_name,
+
+        margin: race.margin_of_victory,
+        cautions: race.number_of_cautions,
+        cautionLaps: race.number_of_caution_laps,
+        averageSpeed: race.average_speed,
+
+        leaderboard: race.results.map(driver => ({
+
+            position: driver.finishing_position,
+            number: driver.car_number,
+            driver: driver.driver_fullname,
+            manufacturer: driver.car_make,
+            team: driver.team_name,
+            sponsor: driver.sponsor,
+            started: driver.starting_position,
+            lapsLed: driver.laps_led,
+            points: driver.points_earned
+
+        }))
+
+    };
 
 }
