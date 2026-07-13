5beba06b83a722f39a42a9fe050727b5d031b99c
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-12 23:29:58 -0300

    Add NASCAR class with live race fetching method

---
+++ services/site.js
@@ -0,0 +1,14 @@
+import {NASCARLive} from "./nascar/live.js";
+import {adaptNascarLive} from "../adapters/nascarLiveAdapter.js";
+
+export class NASCAR{
+
+    static async getLiveRace(){
+
+        const data = await NASCARLive.getLiveRace();
+
+        return adaptNascarLive(data);
+
+    }
+
+}
