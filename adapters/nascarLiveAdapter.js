cf300c1d68232a2908ed6b1da9c4c2db09ec8ae5
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-13 00:06:18 -0300

    Enhance NASCAR live data adaptation logic

--- adapters/nascarLiveAdapter.js
+++ adapters/nascarLiveAdapter.js
@@ -1,7 +1,34 @@
 export function adaptNascarLive(data){
 
-    console.log(data);
+    const manufacturers = {
+        Chv: "Chevrolet",
+        Frd: "Ford",
+        Tyt: "Toyota"
+    };
 
-    return data;
+    return {
+
+        lap: data.lap_number,
+        totalLaps: data.laps_in_race,
+        lapsToGo: data.laps_to_go,
+        flag: data.flag_state,
+
+        leaderboard: data.vehicles
+            .sort((a,b)=>a.running_position-b.running_position)
+            .slice(0,10)
+            .map(car=>({
+
+                position: car.running_position,
+                number: car.vehicle_number,
+                driver: car.driver.full_name,
+                manufacturer: manufacturers[car.vehicle_manufacturer] ?? car.vehicle_manufacturer,
+                sponsor: car.sponsor_name,
+                delta: car.delta,
+                lastLap: car.last_lap_time,
+                bestLap: car.best_lap_time
+
+            }))
+
+    };
 
 }
