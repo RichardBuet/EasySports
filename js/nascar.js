00dbf515999a50805e46035244aa7f4f1e04ff45
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-15 01:26:47 -0300

    Actualizar nascar.js

--- js/nascar.js
+++ js/nascar.js
@@ -1,10 +1,14 @@
 import { renderNascar } from "../modules/pages/nascar.js";
 import { initNascarSelector } from "../modules/nascar/events.js";
+import { initSportsMenu } from "../modules/core/menu.js";
 
 document.addEventListener("DOMContentLoaded",async()=>{
 
     const app=document.getElementById("app");
 
     app.innerHTML=await renderNascar();
+
+    initSportsMenu();
+    initNascarSelector();
 
 });
