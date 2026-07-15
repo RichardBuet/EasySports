c999b9cc1c8ca19aecaef3b96880be8b2c198ea9
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-15 00:03:06 -0300

    Add createNascarSelector function for UI buttons

---
+++ modules/nascar/selector.js
@@ -0,0 +1,11 @@
+export function createNascarSelector(){
+    return `
+        <section class="container">
+            <div class="nascar-selector">
+                <button class="active">🏁 Cup</button>
+                <button>🟢 O'Reilly</button>
+                <button>🚛 Truck</button>
+            </div>
+        </section>
+    `;
+}
