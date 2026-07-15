ebaf1cb0c82e02a20d7241cca004ff41e0272830
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-15 00:56:15 -0300

    Fix return statement in createNascarSelector function

--- modules/nascar/selector.js
+++ modules/nascar/selector.js
@@ -3,20 +3,15 @@ import { NASCAR_SERIES } from "../../config/series.js";
 
 export function createNascarSelector(){
 
-    return 
+    return `
         <section class="container">
             <div id="nascar-selector" class="nascar-selector">
                 <button class="${state.nascarSeries===NASCAR_SERIES.CUP?"active":""}" data-series="1">
-                    🏁 Cup
-                </button>
-
+                    🏁 Cup </button>
                 <button class="${state.nascarSeries===NASCAR_SERIES.OREILLY?"active":""}" data-series="2">
-                    🟢 O'Reilly
-                </button>
-
+                    🟢 O'Reilly </button>
                 <button class="${state.nascarSeries===NASCAR_SERIES.TRUCK?"active":""}" data-series="3">
-                    🚛 Truck
-                </button>
+                    🚛 Truck </button>
             </div>
         </section>
     `;
