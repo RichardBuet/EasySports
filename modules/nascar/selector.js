50e670f6db2e088d425c95d69afd3652657d027e
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-15 00:28:17 -0300

    Refactor NASCAR selector buttons to use state

--- modules/nascar/selector.js
+++ modules/nascar/selector.js
@@ -1,10 +1,22 @@
+import { state } from "../../config/state.js";
+import { NASCAR_SERIES } from "../../config/series.js";
+
 export function createNascarSelector(){
-    return `
+
+    return 
         <section class="container">
             <div id="nascar-selector" class="nascar-selector">
-                <button class="active" data-series="1">🏁 Cup</button>
-                <button data-series="2">🟢 O'Reilly</button>
-                <button data-series="3">🚛 Truck</button>
+                <button class="${state.nascarSeries===NASCAR_SERIES.CUP?"active":""}" data-series="1">
+                    🏁 Cup
+                </button>
+
+                <button class="${state.nascarSeries===NASCAR_SERIES.OREILLY?"active":""}" data-series="2">
+                    🟢 O'Reilly
+                </button>
+
+                <button class="${state.nascarSeries===NASCAR_SERIES.TRUCK?"active":""}" data-series="3">
+                    🚛 Truck
+                </button>
             </div>
         </section>
     `;
