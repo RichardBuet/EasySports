3e701fff2382ca3384a7a5fe28b809784cbfd182
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-14 22:24:36 -0300

    Add createNextEvent function for upcoming events

---
+++ modules/home/nextEvent.js
@@ -0,0 +1,22 @@
+export function createNextEvent(){
+    return `
+        <div class="dashboard-widget">
+            <h2>📅 Próximo evento</h2>
+
+            <div class="today-row">
+                <span>🏁 NASCAR Cup</span>
+                <span>19 Jul</span>
+            </div>
+
+            <div class="today-row">
+                <span>🏟 North Wilkesboro</span>
+                <span>19:00</span>
+            </div>
+
+            <div class="today-row">
+                <span>Estado</span>
+                <span>Programada</span>
+            </div>
+        </div>
+    `;
+}
