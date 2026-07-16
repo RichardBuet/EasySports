30c5cd6a46fe32ef4919aa2f1dbcced15389d709
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-15 23:06:49 -0300

    Refactor nascar selector click event to use async
    Updated the nascar selector to use async refresh function.

--- modules/nascar/events.js
+++ modules/nascar/events.js
@@ -1,21 +1,13 @@
 import { state } from "../../config/state.js";
+import { refreshNascar } from "../../js/nascar.js";
 
 export function initNascarSelector(){
-
     const selector=document.getElementById("nascar-selector");
-
     if(!selector)return;
-
-    selector.addEventListener("click",(event)=>{
-
+    selector.addEventListener("click",async(event)=>{
         const button=event.target.closest("button");
-
         if(!button)return;
-
         state.nascarSeries=Number(button.dataset.series);
-
-        location.reload();
-
+        await refreshNascar();
     });
-
 }
