6d97d4602a9e71f43be7cf652286eff70006b69e
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-17 01:28:17 -0300

    Add NASCAR page rendering function

---
+++ modules/pages/nascar.js
@@ -0,0 +1,9 @@
+import { createLayout } from "../core/layout.js";
+
+export async function renderNascar(){
+
+    return createLayout(`
+        <h1 style="padding:40px;">NASCAR OK</h1>
+    `);
+
+}
