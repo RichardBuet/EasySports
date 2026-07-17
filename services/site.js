06c295bc577f704db7b3dd7332ccb5a515f8a803
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-16 21:28:18 -0300

    Actualizar site.js

--- services/site.js
+++ services/site.js
@@ -4,6 +4,7 @@ import { NASCARWeekend } from "./nascar/weekend.js";
 import { adaptNascarWeekend } from "./adapters/nascarWeekendAdapter.js";
 import { NASCARRaceList } from "./nascar/raceList.js";
 import { adaptNascarRaceList } from "./adapters/nascarRaceListAdapter.js";
+import { state } from "../config/state.js";
 
 export class NASCAR {
 
@@ -11,15 +12,22 @@ export class NASCAR {
         const data = await NASCARLive.getLiveRace();
         return adaptNascarLive(data);
     }
-    static async getWeekend(raceId) {
-        const data = await NASCARWeekend.getWeekend(raceId);
-        return adaptNascarWeekend(data);
-    }
     
-    static async getRaceList(series) {
-        const data = await NASCARRaceList.getRaceList(series);
-        return adaptNascarRaceList(data);
-    }
+static async getWeekend(raceId){
+    const data=await NASCARWeekend.getWeekend(
+        raceId,
+        2026,
+        state.nascarSeries
+    );
+    return adaptNascarWeekend(data);
+}
+    
+    static async getRaceList(){
+    const data=await NASCARRaceList.getRaceList(
+        state.nascarSeries
+    );
+    return adaptNascarRaceList(data);
+}
         
 
 }
