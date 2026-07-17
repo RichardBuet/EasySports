59f36044742b6d5333ac38d035db25a005f67d61
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-16 22:26:00 -0300

    Actualizar hero.js

--- modules/nascar/hero.js
+++ modules/nascar/hero.js
@@ -3,7 +3,7 @@ import { state } from "../../config/state.js";
 import { NASCAR_SERIES } from "../../config/series.js";
 
 export async function createNascarHero(){
-    const race=await NASCAR.getRaceList(state.nascarSeries);
+    const race=await NASCAR.getRaceList();
     const nextRace=race.find(r=>!r.completed);
     const category={
         [NASCAR_SERIES.CUP]:"Cup Series",
