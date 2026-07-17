c5baf009c6192a7888abaa5380adaa5a7a70b06e
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-17 13:46:33 -0300

    Actualizar nascar.js

--- modules/pages/nascar.js
+++ modules/pages/nascar.js
@@ -1,12 +1,14 @@
 import { createLayout } from "../core/layout.js";
 import { createNascarHero } from "../nascar/hero.js";
 import { createNascarSelector } from "../nascar/selector.js";
+import { createNascarDashboard } from "../nascar/dashboard.js";
 
 export async function renderNascar(){
 
     return createLayout(`
         ${await createNascarHero()}
         ${createNascarSelector()}
+        ${await createNascarDashboard()}
     `);
 
 }
