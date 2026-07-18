9c6da0632a16fdea5f4132673d9ad390de253cfe
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-18 12:05:05 -0300

    Add race center to NASCAR render function

--- modules/pages/nascar.js
+++ modules/pages/nascar.js
@@ -1,17 +1,21 @@
 import { createLayout } from "../core/layout.js";
 import { createNascarHero } from "../nascar/hero.js";
 import { createNascarSelector } from "../nascar/selector.js";
+import { createRaceCenter } from "../nascar/raceCenter.js";
 import { createNascarDashboard } from "../nascar/dashboard.js";
 import { createSchedule } from "../nascar/schedule.js";
 import { createDrivers } from "../nascar/drivers.js";
 
 import { setSportTheme } from "../utils/theme.js";
-export async function renderNascar(){
+
+export async function renderNascar() {
+
     setSportTheme("nascar");
 
     return createLayout(`
         ${await createNascarHero()}
         ${createNascarSelector()}
+        ${await createRaceCenter()}
         ${await createNascarDashboard()}
         ${await createSchedule()}
         ${await createDrivers()}
