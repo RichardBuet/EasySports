51ad3ae49b9cbb185edca906450fae67fd960ab6
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-19 14:05:22 -0300

    Add NASCAR standings adapter function

---
+++ services/adapters/nascarStandingsAdapter.js
@@ -0,0 +1,39 @@
+export function adaptNascarStandings(data) {
+
+    return data.map(driver => ({
+
+        position: driver.position,
+
+        driverId: driver.driver_id,
+
+        driver: driver.driver_name,
+
+        number: driver.car_no,
+
+        manufacturer: driver.manufacturer,
+
+        points: driver.points,
+
+        wins: driver.wins,
+
+        poles: driver.poles,
+
+        top5: driver.top_5,
+
+        top10: driver.top_10,
+
+        starts: driver.starts,
+
+        lapsLed: driver.laps_led,
+
+        stagePoints: driver.stage_points,
+
+        deltaLeader: driver.delta_leader,
+
+        deltaNext: driver.delta_next,
+
+        clinched: driver.is_clinch
+
+    }));
+
+}
