7fe5c5507ccccd610bf8b0ae6a062d728b3c1194
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-19 19:29:53 -0300

    Update drivers.js

--- modules/nascar/drivers.js
+++ modules/nascar/drivers.js
@@ -4,8 +4,13 @@ export async function createDrivers() {
 
     const drivers = await NASCAR.getStandingsWithDrivers();
 
-    console.log(drivers);
-
-    return "";
+    return `
+        <section class="drivers">
+            <h2>Drivers</h2>
+            <p>Total: ${drivers.length}</p>
+            <p>Líder: ${drivers[0].driver}</p>
+            <p>Puntos: ${drivers[0].points}</p>
+        </section>
+    `;
 
 }
