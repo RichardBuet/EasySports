c7f53a28c5058033fa02366f0c4afd7897ca40e1
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-19 20:04:21 -0300

    Update nascarDriversAdapter.js

--- services/adapters/nascarDriversAdapter.js
+++ services/adapters/nascarDriversAdapter.js
@@ -1,1 +1,45 @@
+export function adaptNascarDrivers(data) {
 
+    return data.response.map(driver => ({
+
+        driverId: driver.Nascar_Driver_ID,
+
+        driver: driver.Full_Name,
+
+        firstName: driver.First_Name,
+
+        lastName: driver.Last_Name,
+
+        team: driver.Team,
+
+        manufacturer: driver.Manufacturer,
+
+        badge: driver.Badge_Image,
+
+        image: driver.Image,
+
+        imageSmall: driver.Image_Small,
+
+        firesuit: driver.Firesuit_Image,
+
+        hometown: {
+            city: driver.Hometown_City,
+            state: driver.Hometown_State,
+            country: driver.Hometown_Country
+        },
+
+        crewChief: driver.Crew_Chief,
+
+        rookieYear: driver.Rookie_Year_Series_1,
+
+        birthDate: driver.DOB,
+
+        twitter: driver.Twitter_Handle,
+
+        driverPage: driver.Driver_Page,
+
+        series: driver.Driver_Series
+
+    }));
+
+}
