e82978e61370f48fb3ba9c07328550e48cd271d2
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-19 19:17:11 -0300

    Import NASCAR service in createDrivers function
    Import NASCAR service and log drivers standings.

--- modules/nascar/drivers.js
+++ modules/nascar/drivers.js
@@ -1,3 +1,11 @@
-export async function createDrivers(){
+import { NASCAR } from "../../services/site.js";
+
+export async function createDrivers() {
+
+    const drivers = await NASCAR.getStandingsWithDrivers();
+
+    console.log(drivers);
+
     return "";
+
 }
