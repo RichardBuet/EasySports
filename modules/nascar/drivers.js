2f021a24d0b3a4c38a6a06b1d21415e8d572f0ea
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-19 19:33:05 -0300

    Actualizar drivers.js

--- modules/nascar/drivers.js
+++ modules/nascar/drivers.js
@@ -1,15 +1,8 @@
-import { NASCAR } from "../../services/site.js";
-
 export async function createDrivers() {
-
-    const drivers = await NASCAR.getStandingsWithDrivers();
 
     return `
         <section class="drivers">
-            <h2>Drivers</h2>
-            <p>Total: ${drivers.length}</p>
-            <p>Líder: ${drivers[0].driver}</p>
-            <p>Puntos: ${drivers[0].points}</p>
+            <h2>Drivers OK</h2>
         </section>
     `;
 
