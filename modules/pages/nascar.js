ac5b9038c5bb30287b998442a172c207b0c5018b
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-20 00:55:13 -0300

    Include calendar card in NASCAR dashboard
    Added calendar card to the NASCAR render function.

--- modules/pages/nascar.js
+++ modules/pages/nascar.js
@@ -5,9 +5,9 @@ import { createRaceCenter } from "../nascar/raceCenter.js";
 import { createNascarDashboard } from "../nascar/dashboard.js";
 import { createSchedule } from "../nascar/schedule.js";
 import { createDrivers } from "../nascar/drivers.js";
-
+import { createCalendarCard } from "../nascar/calendarCard.js";
 import { setSportTheme } from "../utils/theme.js";
-
+//${await createNascarDashboard()}
 export async function renderNascar() {
 
     setSportTheme("nascar");
@@ -16,7 +16,8 @@ export async function renderNascar() {
         ${await createNascarHero()}
         ${createNascarSelector()}
         ${await createRaceCenter()}
-        ${await createNascarDashboard()}
+        ${await createCalendarCard()}
+        
         ${await createSchedule()}
         ${await createDrivers()}
     `);
