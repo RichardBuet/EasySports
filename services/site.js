889fe88451dc8eb13e47ff7253b6bba577e24b4e
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-19 22:22:08 -0300

    Update site.js

--- services/site.js
+++ services/site.js
@@ -55,7 +55,7 @@ export class NASCAR {
 
                         {
                             icon: "🔴",
-                            value: "LIVE"
+                            value: "LIVE",
                             live: true
                         }
 
