affaafe02e218c1a13e40124b9dbbf7dcde5ea34
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-20 01:36:35 -0300

    Create calendar.js

---
+++ modules/nascar/calendar.js
@@ -0,0 +1,60 @@
+import { NASCAR } from "../../services/site.js";
+import { openModal } from "../components/modal.js";
+
+window.openCalendar = async () => {
+
+    const races = await NASCAR.getRaceList();
+
+    openModal({
+
+        title: "Calendar",
+
+        content: createCalendarContent(races)
+
+    });
+
+};
+
+function createCalendarContent(races) {
+
+    return `
+
+        <div class="driver-header">
+
+            <span>DATE</span>
+            <span>RACE</span>
+            <span>TRACK</span>
+            <span>STATUS</span>
+
+        </div>
+
+        <div class="driver-list">
+
+            ${races.map(race => `
+
+                <div class="driver-row">
+
+                    <span>${new Date(race.date).toLocaleDateString("en-US",{
+                        day:"2-digit",
+                        month:"short"
+                    })}</span>
+
+                    <span>${race.name}</span>
+
+                    <span>${race.track}</span>
+
+                    <strong>
+
+                        ${race.completed ? "🏁" : "📅"}
+
+                    </strong>
+
+                </div>
+
+            `).join("")}
+
+        </div>
+
+    `;
+
+}
