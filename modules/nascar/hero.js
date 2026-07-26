dabf231a492d96ecab5bd5b3795daa14f5d09caf
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-26 20:38:20 -0300

    Update hero.js

--- modules/nascar/hero.js
+++ modules/nascar/hero.js
@@ -3,34 +3,26 @@ import { NASCAR } from "../../services/site.js";
 
 //                ${item.live ? 'onclick="window.openLiveRace()"' : ""}>
 export async function createNascarHero() {
-
-    const heroState = await NASCAR.getHeroState();
-
-const hero = heroState.data;
     
+    const heroState = await NASCAR.getHeroState();
+    const hero = heroState.data;
     const seriesName = NASCAR.getSeriesName();
-
+    
 let heroCategory = `🏁 ${seriesName}`;
 
 switch (heroState.state) {
-
     case "TODAY":
         heroCategory = `🔥 ${seriesName} · HOY HAY CARRERA`;
         break;
-
     case "LIVE":
         heroCategory = `🔴 ${seriesName} · EN VIVO`;
         break;
-
     case "FINISHED":
         heroCategory = `🏁 ${seriesName} · CARRERA FINALIZADA`;
         break;
-
 }
 
-    
     const date = new Date(hero.date);
-
     const raceDate = date.toLocaleDateString(undefined, {
         weekday: "short",
         day: "numeric",
@@ -44,32 +36,34 @@ switch (heroState.state) {
 
     return `
     <section class="nascarHero">
-
         <div class="nascarHeroOverlay"></div>
-
         <div class="nascarHeroContent">
+            // <span class="heroCategory">
+            //     ${heroCategory}
+            // </span>
 
-            <span class="heroCategory">
-                ${heroCategory}
-            </span>
+<span class="heroCategory">
+    ${heroCategory}
+    <span
+        class="heroRaceCenter"
+        onclick="window.openLiveRaceModal()">
+        📊 Race Center
+    </span>
+</span>
 
             <h1>${hero.title}</h1>
             <p>${hero.subtitle}</p>
-
         <div class="heroMeta">
             ${hero.meta.map(item => `
             <div
                 class="heroItem ${item.live ? "heroLive" : ""}"
-
                 ${item.live ? 'onclick="window.openLiveRaceModal()"' : ""}>
                 <span>${item.icon}</span>
                 <strong>${item.value}</strong>
             </div>
         `).join("")}
         </div>
-
         </div>
-
     </section>
 `;
 
