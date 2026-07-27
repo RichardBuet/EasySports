fac5175ac8bd097cc673488aeac759db8679f132
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-27 20:52:28 -0300

    Delete services/espn directory

--- services/espn/index.js
+++
@@ -1,9 +0,0 @@
-import { ESPNCore } from "./core.js";
-import { ESPNSite } from "./site.js";
-import { ESPNFormula1 } from "./formula1.js";
-
-export {
-    ESPNCore,
-    ESPNSite,
-    ESPNFormula1
-};
