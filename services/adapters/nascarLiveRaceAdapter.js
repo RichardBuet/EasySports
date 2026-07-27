36458d216d9fac4e5692eb7453f58d007ccfa0f3
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-26 21:19:12 -0300

    Update nascarLiveRaceAdapter.js

--- services/adapters/nascarLiveRaceAdapter.js
+++ services/adapters/nascarLiveRaceAdapter.js
@@ -5,14 +5,36 @@ export function adaptNascarLiveRace(
     flagData
 ) {
 
-    console.log(liveFeed);
-    console.log(lapTimes);
-    console.log(pitData[0]);
-    console.log(flagData[0]);
-    
     return {
 
-        liveFeed,
+        summary: {
+
+            lap: liveFeed.lap_number,
+            totalLaps: liveFeed.laps_in_race,
+            lapsToGo: liveFeed.laps_to_go,
+            flag: liveFeed.flag_state
+
+        },
+        leaderboard: liveFeed.vehicles.map(vehicle => ({
+
+            position: vehicle.running_position,
+            number: vehicle.vehicle_number,
+            driver: vehicle.driver.full_name,
+            manufacturer: vehicle.vehicle_manufacturer,
+            sponsor: vehicle.sponsor_name,
+            delta: vehicle.delta,
+            averageSpeed: vehicle.average_speed,
+            lastLap: vehicle.last_lap_time,
+            bestLap: vehicle.best_lap_time,
+            pitStops: vehicle.pit_stops.length,
+            lapsLed: vehicle.laps_led.length,
+            fastestLaps: vehicle.fastest_laps_run,
+            startingPosition: vehicle.starting_position,
+            onTrack: vehicle.is_on_track,
+            onDVP: vehicle.is_on_dvp,
+            status: vehicle.status
+
+        })),
         lapTimes,
         pitData,
         flagData
