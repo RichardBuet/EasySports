f99501a9e36b18f26ee547b8ff8d92ef261d07cd
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-27 20:50:29 -0300

    Delete adapters directory

--- adapters/formula1ConstructorAdapter.js
+++
@@ -1,6 +0,0 @@
-import {ESPNCore} from "../services/espn/core.js";
-throw new Error("ESTE ES EL ADAPTER NUEVO");
-export async function adaptConstructorStandings(data){
-console.log("DATA",data);
-return data;
-}
