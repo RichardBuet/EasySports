cea880adced13f172613ea7a423099b6ce7aaec3
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-29 13:10:12 -0300

    Update siteF1.js

--- services/siteF1.js
+++ services/siteF1.js
@@ -1,19 +1,27 @@
-import { getDriverStandings } from "./f1/index.js";
-import { adaptStandings } from "./adapters/f1Adapter.js";
+import {
+    getDriverStandings,
+    getConstructorStandings as fetchConstructorStandings
+} from "./f1/index.js";
+
+import {
+    adaptStandings,
+    adaptConstructorStandings
+} from "./adapters/f1Adapter.js";
 
 export class F1 {
 
     static async getStandings(season = "current") {
+
         const data = await getDriverStandings(season);
         return adaptStandings(data);
+
     }
 
-static async getConstructorStandings(season = "current") {
-    const data = await getConstructorStandings(season);
-    return adaptConstructorStandings(data);
+    static async getConstructorStandings(season = "current") {
 
-}
-    
+        const data = await fetchConstructorStandings(season);
+        return adaptConstructorStandings(data);
 
-  
+    }
+
 }
