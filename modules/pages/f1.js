58cc88d8f74e71b837116b96b009092517be890e
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-29 11:25:41 -0300

    Update f1.js

--- modules/pages/f1.js
+++ modules/pages/f1.js
@@ -1,9 +1,10 @@
 import { createLayout } from "../core/layout.js";
 import { setSportTheme } from "../utils/theme.js";
+import { createStandings } from "../f1/standings.js";
 
 export async function renderF1() {
 
     setSportTheme("f1");
 
-    return createLayout("<h1>F1 one re OK</h1>");
+    return createLayout("<h1>F1 vamos 🔥 one re OK</h1>");
 }
