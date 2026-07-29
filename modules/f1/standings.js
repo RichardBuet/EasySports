c29ff55b110b36eb9b357653adcff6912558917b
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-29 11:42:31 -0300

    Update standings.js

--- modules/f1/standings.js
+++ modules/f1/standings.js
@@ -1,9 +1,20 @@
+import { F1 } from "../../services/siteF1.js";
+
 export async function createStandings() {
 
     const standings = await F1.getStandings();
+
     return `
-        <section>
+        <section class="card">
             <h2>Driver Standings</h2>
+
+            <ul>
+                ${standings.map(driver => `
+                    <li>
+                        ${driver.position}. ${driver.name} - ${driver.points} pts
+                    </li>
+                `).join("")}
+            </ul>
         </section>
     `;
 
