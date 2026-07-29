07fba71654d69a8540d7fbe3746682a8f4ca183d
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-28 23:18:36 -0300

    Update selector.js

--- modules/nascar/selector.js
+++ modules/nascar/selector.js
@@ -8,19 +8,19 @@ export function createNascarSelector(){
             <div id="nascar-selector" class="nascar-selector">
                 <button id="btn-nsc-01" class="${state.nascarSeries === 1 ? "active" : ""}" data-series="1">
                     <img
-                        src="https://www.nascar.com/wp-content/uploads/sites/7/2023/05/10/nascar_cup_series_logo.svg"
+                        src="../../assets/images/nascar_cup_series_logo.svg"
                         alt="Cup Series">
                 </button>
                 
                 <button id="btn-nsc-02" class="${state.nascarSeries === 2 ? "active" : ""}" data-series="2">
                     <img
-                        src="https://www.nascar.com/wp-content/uploads/sites/7/2025/09/30/NOAPS-Primary_FullColor-RGB.svg"
+                        src="../../assets/images/NOAPS-Primary_FullColor-RGB.svg"
                         alt="O'Reilly Series">
                 </button>
                 
                 <button id="btn-nsc-03" class="${state.nascarSeries === 3 ? "active" : ""}"  data-series="3">
                     <img
-                        src="https://www.nascar.com/wp-content/uploads/sites/7/2023/05/10/nascar_craftsman_truck_series_logo.svg"
+                        src="../../assets/images/nascar_craftsman_truck_series_logo.svg"
                         alt="Craftsman Truck Series">
                 </button>
             </div>
