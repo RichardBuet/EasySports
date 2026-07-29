e16ca37106e7e2078eabca140d36ace809967c12
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-29 02:26:16 -0300

    Update standings.js

--- modules/f1/standings.js
+++ modules/f1/standings.js
@@ -2,16 +2,6 @@ import { F1 } from "../../services/siteF1.js";
 
 export async function renderStandings() {
 
-    try {
-
-        const standings = await F1.getStandings();
-
-        console.log("F1 Standings:", standings);
-
-    } catch (error) {
-
-        console.error("Error cargando clasificación F1:", error);
-
-    }
+    document.body.innerHTML = "<h1>🔥 F1 funciona 🔥</h1>";
 
 }
