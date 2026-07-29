94b29993fef0c4b38b2fdcea5a0194b22a208fea
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-29 13:20:15 -0300

    Update siteF1.js

--- services/siteF1.js
+++ services/siteF1.js
@@ -1,11 +1,33 @@
 import {
     getDriverStandings,
-    getConstructorStandings as fetchConstructorStandings
+    getConstructorStandings as fetchConstructorStandings,
+    getSchedule as fetchSchedule,
+    getRaceResults as fetchRaceResults,
+    getQualifying as fetchQualifying,
+    getSprintResults as fetchSprintResults,
+    getDrivers as fetchDrivers,
+    getCircuits as fetchCircuits,
+    getLaps as fetchLaps,
+    getPitStops as fetchPitStops,
+    getFastestLaps as fetchFastestLaps,
+    getSeasons as fetchSeasons,
+    getRounds as fetchRounds
 } from "./f1/index.js";
 
 import {
     adaptStandings,
-    adaptConstructorStandings
+    adaptConstructorStandings,
+    adaptSchedule,
+    adaptResults,
+    adaptQualifying,
+    adaptSprint,
+    adaptDrivers,
+    adaptCircuits,
+    adaptLaps,
+    adaptPitStops,
+    adaptFastestLaps,
+    adaptSeasons,
+    adaptRounds
 } from "./adapters/f1Adapter.js";
 
 export class F1 {
@@ -21,6 +43,83 @@ export class F1 {
 
         const data = await fetchConstructorStandings(season);
         return adaptConstructorStandings(data);
+
+    }
+
+    static async getSchedule(season = "current") {
+
+        const data = await fetchSchedule(season);
+        return adaptSchedule(data);
+
+    }
+
+    static async getResults(season = "current", round = "last") {
+
+        const data = await fetchRaceResults(season, round);
+        return adaptResults(data);
+
+    }
+
+    static async getQualifying(season = "current", round = "last") {
+
+        const data = await fetchQualifying(season, round);
+        return adaptQualifying(data);
+
+    }
+
+    static async getSprint(season = "current", round = "last") {
+
+        const data = await fetchSprintResults(season, round);
+        return adaptSprint(data);
+
+    }
+
+    static async getDrivers(season = "current") {
+
+        const data = await fetchDrivers(season);
+        return adaptDrivers(data);
+
+    }
+
+    static async getCircuits(season = "current") {
+
+        const data = await fetchCircuits(season);
+        return adaptCircuits(data);
+
+    }
+
+    static async getLaps(season = "current", round = "last") {
+
+        const data = await fetchLaps(season, round);
+        return adaptLaps(data);
+
+    }
+
+    static async getPitStops(season = "current", round = "last") {
+
+        const data = await fetchPitStops(season, round);
+        return adaptPitStops(data);
+
+    }
+
+    static async getFastestLaps(season = "current", round = "last") {
+
+        const data = await fetchFastestLaps(season, round);
+        return adaptFastestLaps(data);
+
+    }
+
+    static async getSeasons() {
+
+        const data = await fetchSeasons();
+        return adaptSeasons(data);
+
+    }
+
+    static async getRounds(season = "current") {
+
+        const data = await fetchRounds(season);
+        return adaptRounds(data);
 
     }
 
