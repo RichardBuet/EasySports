318b44831d7219f9e7bc6b16f26c522a0b8bfbdb
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-29 01:49:10 -0300

    Export getDriverStandings function
    Export getDriverStandings from standings.js.

--- services/index.js
+++ services/index.js
@@ -1,1 +1,6 @@
-
+export { getDriverStandings } from "./standings.js";
+// export { getRaceSchedule } from "./schedule.js";
+// export { getRaceResults } from "./results.js";
+// export { getQualifyingResults } from "./qualifying.js";
+// export { getLapTimes } from "./laps.js";
+// export { getPitStops } from "./pitStops.js";
