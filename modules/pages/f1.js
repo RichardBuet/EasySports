508670d4d12474b379e9c5b9d75f21733952e3d6
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-31 14:48:43 -0300

    Remove createExtras() from renderF1 function
    Removed the call to createExtras() from the renderF1 function.

--- modules/pages/f1.js
+++ modules/pages/f1.js
@@ -7,19 +7,15 @@ import { createSelector } from "../f1/selector.js";
 import { createRaceCenter } from "../f1/raceCenter.js";
 import { createCalendarCard } from "../f1/calendarCard.js";
 import { createDriversCard } from "../f1/driversCard.js";
-
+//  ${await createExtras()}
 export async function renderF1() {
 
     setSportTheme("f1");
 
 return createLayout(`
     ${await createHero()}
-
     ${createSelector()}
-
     ${await createDashboard()}
-
-    ${await createExtras()}
 `);
 
 }
