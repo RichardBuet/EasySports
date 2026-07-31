78b8cf50785486c089a635de6d4cfc79b072c2c1
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-31 13:58:49 -0300

    Update f1.js

--- js/f1.js
+++ js/f1.js
@@ -1,10 +1,12 @@
 import { renderF1 } from "../modules/pages/f1.js";
+import { initSportsMenu } from "../modules/core/menu.js";
 
 const app = document.getElementById("app");
 
 export async function refreshF1() {
 
     app.innerHTML = await renderF1();
+initSportsMenu() ;
 
 }
 
