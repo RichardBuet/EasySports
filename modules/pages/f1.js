0c64a136f8eeb6aa89683c655da3fc10364cacb4
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-07-31 15:30:00 -0300

    Update f1.js

--- modules/pages/f1.js
+++ modules/pages/f1.js
@@ -2,22 +2,48 @@ import { createLayout } from "../core/layout.js";
 import { setSportTheme } from "../utils/theme.js";
 
 import { createHero } from "../f1/hero.js";
-import { createDashboard } from "../f1/dashboard.js";
 import { createSelector } from "../f1/selector.js";
+
 import { createRaceCenter } from "../f1/raceCenter.js";
-import { createCalendarCard } from "../f1/calendarCard.js";
-import { createDriversCard } from "../f1/driversCard.js";
-//  ${await createExtras()}
+import { createStandings } from "../f1/standings.js";
+import { createConstructorStandings } from "../f1/constructorStandings.js";
+import { createResults } from "../f1/results.js";
+import { createQualifying } from "../f1/qualifying.js";
+import { createDrivers } from "../f1/drivers.js";
+import { createCalendar } from "../f1/calendar.js";
+
 export async function renderF1() {
 
     setSportTheme("f1");
 
-return createLayout(`
-    ${await createHero()}
-    ${createSelector()}
-    ${await createDashboard()}
-`);
+    return createLayout(`
+
+        ${await createHero()}
+
+        ${createSelector()}
+
+        <section class="dashboard">
+
+            <div class="dashboard-grid">
+
+                ${await createRaceCenter()}
+
+                ${await createStandings()}
+
+                ${await createConstructorStandings()}
+
+                ${await createResults()}
+
+                ${await createQualifying()}
+
+                ${await createDrivers()}
+
+                ${await createCalendar()}
+
+            </div>
+
+        </section>
+
+    `);
 
 }
-    
-
