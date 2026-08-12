cf37d58a1a5af1013234f6d5eb5ac49a88d06315
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-12 11:53:02 -0300

    Update live.js

--- services/nascar/live.js
+++ services/nascar/live.js
@@ -1,7 +1,7 @@
 import { fetchJSON } from "../shared/fetch.js";
 
 //const LIVE_URL = "https://cf.nascar.com/live/feeds/live-feed.json";
-const LIVE_URL = "https://github.com/RichardBuet/EasySports/data/nascar/live/live-feed.json";
+const LIVE_URL = "https://raw.githubusercontent.com/RichardBuet/EasySports/main/data/nascar/live/live-feed.json";
 
 export class NASCARLive {
 
