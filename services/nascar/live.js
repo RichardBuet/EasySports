8fec7f79f6a05b1cdee7d1bf604553a40dfb1318
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-14 18:31:00 -0300

    Update live.js

--- services/nascar/live.js
+++ services/nascar/live.js
@@ -1,7 +1,7 @@
 import { fetchJSON } from "../shared/fetch.js";
-
-//const LIVE_URL = "https://cf.nascar.com/live/feeds/live-feed.json";
-const LIVE_URL = "https://raw.githubusercontent.com/RichardBuet/EasySports/main/data/nascar/live/live-feed.json";
+// Determina la información de carreras en vivo con opción real o DevES
+const LIVE_URL = "https://cf.nascar.com/live/feeds/live-feed.json";
+//const LIVE_URL = "https://raw.githubusercontent.com/RichardBuet/EasySports/main/data/nascar/live/live-feed.json";
 
 export class NASCARLive {
 
