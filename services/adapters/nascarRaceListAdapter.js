a49cf6e63ef2fda93bc9f4bcf5696a79b30dcb91
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-14 22:54:20 -0300

    Update nascarRaceListAdapter.js

--- services/adapters/nascarRaceListAdapter.js
+++ services/adapters/nascarRaceListAdapter.js
@@ -2,28 +2,44 @@ export function adaptNascarRaceList(data) {
 
     return data.map(race => {
 
-        const completed = race.winner_driver_id !== null;
+        const completed =
+            race.winner_driver_id !== null;
+
+        const raceEvent =
+            race.schedule?.find(
+                event => event.event_name === "Race"
+            );
+
+        const qualifyingEvent =
+            race.schedule?.find(
+                event =>
+                    event.event_name === "Qualifying (Impound)"
+            );
 
         return {
 
             raceId: race.race_id,
+
             seriesId: race.series_id,
 
             name: race.race_name,
+
             track: race.track_name,
 
-date: `${race.schedule?.find(
-    event => event.event_name === "Race"
-)?.start_time_utc}Z`,
+            date: raceEvent?.start_time_utc
+                ? `${raceEvent.start_time_utc}Z`
+                : null,
 
-qualifyingDate: `${race.schedule?.find(
-    event => event.event_name === "Qualifying (Impound)"
-)?.start_time_utc}Z`,
+            qualifyingDate: qualifyingEvent?.start_time_utc
+                ? `${qualifyingEvent.start_time_utc}Z`
+                : null,
 
             scheduledLaps: race.scheduled_laps,
+
             actualLaps: race.actual_laps,
 
             scheduledDistance: race.scheduled_distance,
+
             actualDistance: race.actual_distance,
 
             winnerDriverId: race.winner_driver_id,
@@ -33,11 +49,15 @@ qualifyingDate: `${race.schedule?.find(
             schedule: race.schedule,
 
             television: race.television_broadcaster,
+
             radio: race.radio_broadcaster,
 
             completed,
 
-            status: completed ? "completed" : "upcoming"
+            status:
+                completed
+                    ? "completed"
+                    : "upcoming"
 
         };
 
