86aba829efd92f20af24eca5b34ac8b9ec0555db
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-15 00:12:13 -0300

    Refactor refreshNascar function and improve formatting

--- js/nascar.js
+++ js/nascar.js
@@ -1,20 +1,43 @@
 import { renderNascar } from "../modules/pages/nascar.js";
 import { initSportsMenu } from "../modules/core/menu.js";
 import { initNascarEvents } from "../modules/nascar/events.js";
+import { centerCalendarCurrentRace } from "../modules/nascar/calendarCard.js";
+import { NASCAR } from "../services/site.js";
+
 import "../modules/nascar/liveFullModal.js";
 import "../modules/nascar/calendar.js";
 import "../modules/nascar/resultModal.js";
 import "../modules/nascar/drivers.js";
 import "../modules/nascar/liveRaceModal.js";
 import "../modules/nascar/raceInfoModal.js";
-const app=document.getElementById("app");
 
-export async function refreshNascar(){
-    app.innerHTML=await renderNascar();
+const app = document.getElementById("app");
+
+export async function refreshNascar() {
+
+    app.innerHTML = await renderNascar();
+
     initSportsMenu();
     initNascarEvents();
+
+    requestAnimationFrame(() => {
+
+        requestAnimationFrame(async () => {
+
+            const timeline = await NASCAR.getTimeline();
+
+            centerCalendarCurrentRace(
+                timeline.currentIndex
+            );
+
+        });
+
+    });
+
 }
 
-document.addEventListener("DOMContentLoaded",async()=>{
+document.addEventListener("DOMContentLoaded", async () => {
+
     await refreshNascar();
+
 });
