e9af084b44ed9857810d163325fb2d95f2a24ca5
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-19 23:42:21 -0300

    Add optional image parameter to createCard function

--- modules/components/card.js
+++ modules/components/card.js
@@ -1,6 +1,6 @@
-export function createCard(title, description, link = "#") {
+export function createCard(title, description, link = "#", image = null) {
     return `
-        <article class="card">
+        <article class="card"${image ? ` style="--card-bg: url('${image}')"` : ""}>
             <h3>${title}</h3>
             <p>${description}</p>
             <a href="${link}" class="button">Ver más</a>
