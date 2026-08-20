73f79c20991017f846557f3279f981cc3674c01d
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-20 14:01:37 -0300

    Initialize F1 dashboard modals in refreshF1 function

--- js/f1.js
+++ js/f1.js
@@ -1,17 +1,15 @@
 import { renderF1 } from "../modules/pages/f1.js";
 import { initSportsMenu } from "../modules/core/menu.js";
+import { initF1DashboardModals } from "../modules/f1/dashboard.js";
 
 const app = document.getElementById("app");
 
 export async function refreshF1() {
-
     app.innerHTML = await renderF1();
-initSportsMenu() ;
-
+    initSportsMenu();
+    initF1DashboardModals();
 }
 
 document.addEventListener("DOMContentLoaded", async () => {
-
     await refreshF1();
-
 });
