d63fb11abef3d6262a5bc1d33d7e1099e7281f94
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-20 12:33:19 -0300

    Update results.js

--- modules/f1/results.js
+++ modules/f1/results.js
@@ -1,23 +1,20 @@
 import { F1 } from "../../services/siteF1.js";
 
 export async function createResults() {
-
     const results = await F1.getResults();
-
     return `
-        <section class="card">
-
+        <section class="card f1-results">
             <h2>🏁 Last Race</h2>
-
             <ul>
-                ${results.map(driver => `
+                ${results.map(result => `
                     <li>
-                        ${driver.position}. ${driver.driver.fullName}
+                        <span>${result.position}.</span>
+                        <strong>${result.driver.fullName}</strong>
+                        <span>${result.constructor.name}</span>
+                        <span>${result.points} pts</span>
                     </li>
                 `).join("")}
             </ul>
-
         </section>
     `;
-
 }
