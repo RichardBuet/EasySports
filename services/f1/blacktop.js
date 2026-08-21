9a01632468c0dcd5002f68b3a844ae69a558e3d2
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-21 19:30:13 -0300

    Create blacktop.js

---
+++ services/f1/blacktop.js
@@ -0,0 +1,14 @@
+import { fetchJSON } from "../shared/fetch.js";
+
+const BASE_URL =
+    "/EasySports/data/f1-blacktop.json";
+
+
+export async function getBlacktopEvents() {
+
+    const data =
+        await fetchJSON(BASE_URL);
+
+    return data;
+
+}
