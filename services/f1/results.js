6601050d06d49e86b86837148e661748b23d5f57
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-25 15:30:04 -0300

    Update results.js

--- services/f1/results.js
+++ services/f1/results.js
@@ -1,14 +1,14 @@
 import { fetchJSON } from "../shared/fetch.js";
 
-const BASE_URL = "https://api.jolpi.ca/ergast/f1";
+const BASE_URL="https://api.jolpi.ca/ergast/f1";
 
 export async function getRaceResults(
-    season = "current",
-    round = "last"
-) {
-
-    const url = `${BASE_URL}/${season}/${round}/results/?format=json`;
+    season="current",
+    round=null
+){
+    const url=round
+        ? `${BASE_URL}/${season}/${round}/results/?format=json`
+        : `${BASE_URL}/${season}/results/?format=json`;
 
     return await fetchJSON(url);
-
 }
