3eda655eb7e1e0e22af93962f707d389d7a57266
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-24 21:49:27 -0300

    Refactor imports in f1.js for better structure

--- modules/pages/f1.js
+++ modules/pages/f1.js
@@ -1,16 +1,12 @@
-import { F1 } from "../../services/siteF1.js";
-
-import { createLayout } from "../layout.js";
+import { createLayout } from "../core/layout.js";
 import { setSportTheme } from "../utils/theme.js";
 
-import { createHero } from "./f1/hero.js";
-import { createDashboard } from "./f1/dashboard.js";
-import { createRaceCenter } from "./f1/raceCenter.js";
+import { createHero } from "../f1/hero.js";
+import { createDashboard } from "../f1/dashboard.js";
+import { createRaceCenter } from "../f1/raceCenter.js";
 
 export async function renderF1() {
-
     setSportTheme("f1");
-
     const [
         hero,
         dashboard,
@@ -20,13 +16,9 @@ export async function renderF1() {
         createDashboard(),
         createRaceCenter()
     ]);
-
     return createLayout(`
         ${hero}
-
         ${dashboard}
-
         ${raceCenter}
     `);
-
 }
