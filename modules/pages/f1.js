8ad0305f0a3f8f213facaf4e09c46e37742dafed
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-08-24 21:32:01 -0300

    Update f1.js

--- modules/pages/f1.js
+++ modules/pages/f1.js
@@ -1,63 +1,32 @@
 import { F1 } from "../../services/siteF1.js";
 
-import { createLayout } from "../core/layout.js";
+import { createLayout } from "../layout.js";
 import { setSportTheme } from "../utils/theme.js";
-import { createHero } from "../f1/hero.js";
-import { createDashboard } from "../f1/dashboard.js";
-import { createSelector } from "../f1/selector.js";
-import { createRaceCenter } from "../f1/raceCenter.js";
-import { createStandings } from "../f1/standings.js";
-import { createConstructorStandings } from "../f1/constructorStandings.js";
-import { createResults } from "../f1/results.js";
-import { createQualifying } from "../f1/qualifying.js";
-import { createDrivers } from "../f1/drivers.js";
-import { createCalendar } from "../f1/calendar.js";
+
+import { createHero } from "./f1/hero.js";
+import { createDashboard } from "./f1/dashboard.js";
+import { createRaceCenter } from "./f1/raceCenter.js";
 
 export async function renderF1() {
 
-
-    
-
-
-
-    
     setSportTheme("f1");
+
     const [
         hero,
         dashboard,
-        raceCenter,
-        standings,
-        constructorStandings,
-        results,
-        qualifying,
-        drivers,
-        calendar
+        raceCenter
     ] = await Promise.all([
         createHero(),
         createDashboard(),
-        createRaceCenter(),
-        createStandings(),
-        createConstructorStandings(),
-        createResults(),
-        createQualifying(),
-        createDrivers(),
-        createCalendar()
+        createRaceCenter()
     ]);
 
     return createLayout(`
         ${hero}
+
         ${dashboard}
-        ${createSelector()}
-        <section class="dashboard">
-            <div class="dashboard-grid">
-                ${raceCenter}
-                ${standings}
-                ${constructorStandings}
-                ${results}
-                ${qualifying}
-                ${drivers}
-                ${calendar}
-            </div>
-        </section>
+
+        ${raceCenter}
     `);
+
 }
