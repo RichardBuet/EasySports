da87ff3378dd4f691ce3c860d114b08026af0465
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-09-12 23:28:22 -0300

    Update bunner.js

--- modules/components/bunner.js
+++ modules/components/bunner.js
@@ -1,14 +1,17 @@
 export function createBunner() {
     return `
         <section class="bunner">
-
-            <img
-                
-                src="assets/images/Bunner-EasySports.png"
-                alt="EasySports Banner"
-                class="hero-bunner"
-            >
-
+            <picture>
+                <source
+                    media="(orientation: portrait)"
+                    srcset="assets/images/EasySports-backmovil.png"
+                >
+                <img
+                    src="assets/images/Bunner-EasySports.png"
+                    alt="EasySports Banner"
+                    class="hero-bunner"
+                >
+            </picture>
         </section>
     `;
 }
