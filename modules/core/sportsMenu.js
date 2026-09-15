605956c9120bda25db08eaae1e4a65c4c29f8008
RichardBuet <105518735+RichardBuet@users.noreply.github.com>
2026-09-14 23:33:51 -0300

    Update sports menu with new images and entries

--- modules/core/sportsMenu.js
+++ modules/core/sportsMenu.js
@@ -2,48 +2,44 @@ const sports = [
     {
         label: "Fórmula 1",
         icon: "🏎",
+        image: "/EasySports/assets/logos/f1-new.svg",
         href: "https://richardbuet.github.io/EasySports/pages/formula1.html"
     },
     {
         label: "Nascar",
         icon: "🏁",
+        image: "/EasySports/assets/logos/nascar.svg",
         href: "https://richardbuet.github.io/EasySports/pages/nascar.html"
     },
     {
         label: "MotoGP",
         icon: "🏍",
+        image: "/EasySports/assets/logos/motogp.svg",
         href: "#"
     },
     {
         label: "IndyCar",
         icon: "🚗",
+        image: "/EasySports/assets/logos/indycar-series.svg",
         href: "#"
     },
-    {
-        label: "Fórmula E",
-        icon: "⚡",
-        href: "#"
-    },
-    {
-        label: "WEC",
-        icon: "🏆",
-        href: "#"
-    },
-    {
-        label: "Copa del Mundo",
-        icon: "⚽",
-        href: "https://richardbuet.github.io/whatscup/"
-    },
-    // {
-    //     label: "NBA",
-    //     icon: "🏀",
-    //     href: "#"
-    // },
-    // {
-    //     label: "Tenis",
-    //     icon: "🎾",
-    //     href: "#"
-    // }
+{
+    label: "Fórmula E",
+    icon: "⚡",
+    image: "/EasySports/assets/logos/formula-e.png",
+    href: "#"
+},
+{
+    label: "WEC",
+    icon: "🏆",
+    image: "/EasySports/assets/logos/wec.svg",
+    href: "#"
+},
+{
+    label: "Copa del Mundo",
+    icon: "⚽",
+    href: "https://richardbuet.github.io/whatscup/"
+},
 ];
 
 export function getSports() {
